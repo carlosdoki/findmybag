74ab3531873c74cf59a4b036eedb174e3e8ad0ee
Carlos Doki <carlosdoki@gmail.com>
2023-05-07 11:11:56 -0300

    Commit

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'dart:async';
+import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:findmybag/services/location_client.dart';
@@ -36,7 +37,7 @@ class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
     _listenLocation();
     Timer.periodic(const Duration(seconds: 3), (timer) {
       _listenLocation();
-      //_getMalas();
+      _getMalas();
       // _calculateDistance();
     });
   }
@@ -101,6 +102,26 @@ class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
                             Marker(
                               point: _currPosition!,
                               builder: (context) => const Icon(Icons.person),
+                            ),
+                          ],
+                        ),
+                      //Mala
+                      PolylineLayer(
+                        polylineCulling: false,
+                        polylines: [
+                          Polyline(
+                            points: _malas,
+                            color: Colors.brown,
+                            strokeWidth: 4,
+                          ),
+                        ],
+                      ),
+                      if (_currMala != null)
+                        MarkerLayer(
+                          markers: [
+                            Marker(
+                              point: _currMala!,
+                              builder: (context) => const Icon(Icons.backpack),
                             ),
                           ],
                         ),
@@ -200,13 +221,16 @@ class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   AppText(
-                                    text: 'Bagagem',
+                                    text: 'Bagagem de mao',
                                   ),
                                   SizedBox(
                                     height: 10,
                                   ),
-                                  AppText(
-                                    text: '123',
+                                  Text(
+                                    'Última localidade:',
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                    ),
                                   ),
                                 ],
                               ),
@@ -228,10 +252,10 @@ class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
         .doc('mala')
         .collection('rastreamento');
 
-    final _posicoesCollection =
-        malasCollection.doc(malasCollection.id).collection('logs').limit(1);
-    // _currMala = LatLng(_posicoesCollection.snapshots()['latitute'],
-    //     _posicoesCollection['latitute'].toString());
+    Random random = new Random();
+
+    _malas.add(LatLng(-23.4902178391228 + random.nextDouble() * 100,
+        -46.83564241296433 + random.nextDouble() * 100));
   }
 }
 
@@ -250,250 +274,3 @@ saveLocationData(double latitude, double longitude) {
       .then((value) => print('Location data added'))
       .catchError((error) => print('Failed to add location data: $error'));
 }
-
-// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-// import 'dart:async';
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:findmybag/services/location_client.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_map/flutter_map.dart';
-// import 'package:latlong2/latlong.dart';
-
-// import '../widgets/app_text.dart';
-
-// class MapScreen extends StatefulWidget {
-//   static const id = 'map_screen.dart';
-//   const MapScreen({
-//     super.key,
-//   });
-
-//   @override
-//   State<MapScreen> createState() => _MapScreenState();
-// }
-
-// class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
-//   final _locationClient = LocationClient();
-//   final _mapController = MapController();
-//   final _points = <LatLng>[];
-//   final _malas = <LatLng>[];
-//   LatLng? _currMala;
-//   LatLng? _currPosition;
-//   LatLng? _currPositionAnt;
-//   bool _isServiceRunning = false;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _locationClient.init();
-//     _listenLocation();
-//     Timer.periodic(const Duration(seconds: 3), (timer) {
-//       _listenLocation();
-//       _getMalas();
-//       // _calculateDistance();
-//     });
-//   }
-
-//   void _listenLocation() async {
-//     if (!_isServiceRunning && await _locationClient.isServiceEnabled()) {
-//       _isServiceRunning = true;
-//       _locationClient.locationStream.listen((event) async {
-//         setState(() {
-//           _currPosition = event;
-//         });
-//         _points.add(_currPosition!);
-//         if (_currPositionAnt != _currPosition) {
-//           _currPositionAnt = _currPosition;
-//           await saveLocationData(
-//               _currPosition!.latitude, _currPosition!.longitude);
-//         }
-//       });
-//     } else {
-//       _isServiceRunning = false;
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     TabController _tabController = TabController(length: 3, vsync: this);
-//     return Scaffold(
-//       body: Center(child: LayoutBuilder(builder: (context, _) {
-//         return _currPosition == null
-//             ? const CircularProgressIndicator()
-//             : Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Container(
-//                     height: 500,
-//                     width: double.maxFinite,
-//                     // margin: const EdgeInsets.only(top: 60, right: 20, left: 20),
-//                     child: FlutterMap(
-//                       mapController: _mapController,
-//                       options: MapOptions(
-//                         center: _currPosition,
-//                         zoom: 18,
-//                       ),
-//                       children: [
-//                         TileLayer(
-//                           urlTemplate:
-//                               'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-//                           userAgentPackageName: 'com.example.app',
-//                         ),
-//                         PolylineLayer(
-//                           polylineCulling: false,
-//                           polylines: [
-//                             Polyline(
-//                               points: _points,
-//                               color: Colors.blue,
-//                               strokeWidth: 4,
-//                             ),
-//                           ],
-//                         ),
-//                         if (_currPosition != null)
-//                           MarkerLayer(
-//                             markers: [
-//                               Marker(
-//                                 point: _currPosition!,
-//                                 builder: (context) => const Icon(Icons.person),
-//                               ),
-//                             ],
-//                           ),
-//                       ],
-//                     ),
-//                   ),
-//                   const SizedBox(height: 20),
-//                      Container(
-//             height: 200,
-//             width: double.maxFinite,
-//             child: TabBarView(
-//               controller: _tabController,
-//               children: [
-//                 Padding(
-//                   padding: const EdgeInsets.only(left: 20, top: 20),
-//                   child: Container(
-//                     child: Row(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Container(
-//                           width: 80,
-//                           height: 80,
-//                           color: Colors.blue,
-//                         ),
-//                         SizedBox(width: 5),
-//                         Column(
-//                           mainAxisAlignment: MainAxisAlignment.start,
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             AppText(
-//                               text: 'Teste',
-//                             ),
-//                             SizedBox(
-//                               height: 10,
-//                             ),
-//                             AppText(
-//                               text: '123',
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.only(left: 20, top: 20),
-//                   child: Container(
-//                     child: Row(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Container(
-//                           width: 80,
-//                           height: 80,
-//                           color: Colors.blue,
-//                         ),
-//                         SizedBox(width: 5),
-//                         Column(
-//                           mainAxisAlignment: MainAxisAlignment.start,
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             AppText(
-//                               text: 'Bagagem',
-//                             ),
-//                             SizedBox(
-//                               height: 10,
-//                             ),
-//                             AppText(
-//                               text: '123',
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.only(left: 20, top: 20),
-//                   child: Container(
-//                     child: Row(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Container(
-//                           width: 80,
-//                           height: 80,
-//                           color: Colors.blue,
-//                         ),
-//                         SizedBox(width: 5),
-//                         Column(
-//                           mainAxisAlignment: MainAxisAlignment.start,
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             AppText(
-//                               text: 'aaa',
-//                             ),
-//                             SizedBox(
-//                               height: 10,
-//                             ),
-//                             AppText(
-//                               text: '123',
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//                 ],
-//               )
-                           
-//       })),
-//     );
-//   }
-
-//   void _getMalas() {
-//     final CollectionReference malasCollection = FirebaseFirestore.instance
-//         .collection('locations')
-//         .doc('mala')
-//         .collection('rastreamento');
-
-//     final _posicoesCollection =
-//         malasCollection.doc(malasCollection.id).collection('logs').limit(1);
-//     // _currMala = LatLng(_posicoesCollection.snapshots()['latitute'],
-//     //     _posicoesCollection['latitute'].toString());
-//   }
-// }
-
-// saveLocationData(double latitude, double longitude) {
-//   CollectionReference locations = FirebaseFirestore.instance
-//       .collection('locations')
-//       .doc('celular')
-//       .collection('historico');
-
-//   return locations
-//       .add({
-//         'latitude': latitude,
-//         'longitude': longitude,
-//         'timestamp': FieldValue.serverTimestamp()
-//       })
-//       .then((value) => print('Location data added'))
-//       .catchError((error) => print('Failed to add location data: $error'));
-// }
